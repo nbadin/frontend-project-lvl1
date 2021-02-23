94ae58c2b1ad57f82793b14cc23ddb42bebf4dc2
Nikita Badin <nbadin011@gmail.com>
2021-02-23 21:38:23 +0300

    step 4 not shure

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,5 @@
 import readlineSync from 'readline-sync';
 
-
-
 export const greetin = () => {
     const name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`); 
