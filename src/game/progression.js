2e6e830cb516d197954961ef4c130ddf0d9ea9ae
Nikita Badin <nbadin011@gmail.com>
2021-03-13 15:06:32 +0300

    Refactoring 5.1

--- src/game/progression.js
+++ src/game/progression.js
@@ -4,7 +4,7 @@ import runGame, { roundCount } from '../index.js';
 const getQuestion = (first, step, hiddenNumberIndex) => {
   const progression = [];
   for (let i = 0; i < 10; i += 1) {
-    const element = i === hiddenNumberIndex ? '..' : String(first + i * step);
+    const element = i === hiddenNumberIndex ? '..' : String(first + (i * step));
     progression.push(element);
   }
   return progression.join(' ');
@@ -15,7 +15,7 @@ const generateRound = () => {
   const step = generateNumber(1, 10);
   const hiddenNumberIndex = generateNumber(1, 10);
   const question = getQuestion(first, step, hiddenNumberIndex);
-  const answer = String(first + step * hiddenNumberIndex);
+  const answer = String(first + (step * hiddenNumberIndex));
 
   return [question, answer];
 };
